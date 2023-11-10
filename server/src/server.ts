3fdfa12c5c1131b83dcb273330bb1372fdc98111
Nithin Tomy <nithintomy2255@gmail.com>
2023-11-10 16:47:08 +0530

    changed socket origin

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 export const io = new Server(server, {
     cors: {
-      origin: 'http://localhost:3000',
+      origin: 'https://learnify.website',
       methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
       credentials: true,
     },
