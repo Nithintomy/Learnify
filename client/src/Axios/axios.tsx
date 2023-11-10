8cf04548cef5c62c6b17a17a8dfae1276b64a339
Nithin Tomy <nithintomy2255@gmail.com>
2023-11-10 12:18:27 +0530

    Api urls Changed

--- client/src/Axios/axios.tsx
+++ client/src/Axios/axios.tsx
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 
 const axiosInstance =axios.create({
-    baseURL:"http://localhost:5000",
+    // baseURL:"http://localhost:5000",
+     baseURL:"https://learnify.website",
     timeout:5000
 })
 
