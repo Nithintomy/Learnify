e54637806bd29fb2a5194ea4e24aa89c0fe0aa2e
Nithintomy <nithintomy2255@gmail.com>
2024-04-25 00:26:25 +0530

    render link changes

--- client/src/Axios/axios.tsx
+++ client/src/Axios/axios.tsx
@@ -4,7 +4,8 @@ import axios from 'axios';
 
 const axiosInstance =axios.create({
     // baseURL:"http://localhost:5000",
-     baseURL:"https://learnify.website",
+    //  baseURL:"https://learnify.website",
+     baseURL:"https://learnifybackend.onrender.com",
     timeout:5000
 })
 
