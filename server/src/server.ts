d6dbf286e2fee0fe390a9d5c8f5bf77000945c1c
Nithintomy <nithintomy2255@gmail.com>
2024-04-24 11:18:41 +0530

    render root setuped

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 export const io = new Server(server, {
     cors: {
-      origin: 'https://learnify.website',
+      origin: ['https://learnify.website','https://learnify.onrender.com'],
       methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
       credentials: true,
     },
